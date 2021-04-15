815678c1ac51501be487f14481549b6c4a94ab71
Lucas Morón <lucasmoron0512@gmail.com>
2021-04-15 16:17:18 -0300

    feat: Hola mundo implementado con spring boot

---
+++ backend-desapp-apl/backend-desapp-apl/src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/HelloWorld.java
@@ -0,0 +1,22 @@
+package ar.edu.unq.desapp.grupoL012021.backenddesappapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorld {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String hola() {
+        return "Hola mundo";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+}
--- backend-desapp-apl/backend-desapp-apl/src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/model/Reviewable.java
+++ backend-desapp-apl/backend-desapp-apl/src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/model/Reviewable.java
@@ -1,7 +1,5 @@
 package ar.edu.unq.desapp.grupoL012021.backenddesappapl.model;
 
 public class Reviewable {
-    public static void main(String[] args){
-    System.out.println("hola mundo");
-    }
+    public static void main(String[] args){ System.out.println("hola mundo"); }
 }
