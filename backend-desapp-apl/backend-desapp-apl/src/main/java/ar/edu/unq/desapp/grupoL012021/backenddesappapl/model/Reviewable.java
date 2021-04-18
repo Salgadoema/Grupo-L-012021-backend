5f2195fcde8797863eed7631227b42ad1097a742
Emanuel <salgadoema@gmail.com>
2021-04-18 19:26:34 -0300

    feat: hola mundo en local host 7000

--- backend-desapp-apl/backend-desapp-apl/src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/model/Reviewable.java
+++ backend-desapp-apl/backend-desapp-apl/src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/model/Reviewable.java
@@ -1,7 +1,43 @@
 package ar.edu.unq.desapp.grupoL012021.backenddesappapl.model;
+/*//import io.javalin.Javalin;
+
 
 public class Reviewable {
     public static void main(String[] args){
-    System.out.println("hola mundo");
+  //      Javalin app=Javalin.create().start(7000);
+    //    app.get("/", ctx ->ctx.result("Hello World"));
+        System.out.println("hola mundo");
+
     }
+}*/
+import java.util.Arrays;
+
+        import org.springframework.boot.CommandLineRunner;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.ApplicationContext;
+        import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Reviewable  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reviewable.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
 }
