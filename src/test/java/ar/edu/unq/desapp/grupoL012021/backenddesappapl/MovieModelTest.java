8ee2b4ec095c927d1fb08ab068d6b01f4e7ab1ad
Lucas Morón <lucasmoron0512@gmail.com>
2021-04-21 14:46:04 -0300

    Feat: test basico para probar con travis

---
+++ src/test/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/MovieModelTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.desapp.grupoL012021.backenddesappapl;
+
+
+import ar.edu.unq.desapp.grupoL012021.backenddesappapl.model.Movie;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MovieModelTest {
+
+    private Movie testMovie = new Movie(1, "Die Hard");
+
+    @Test
+    @DisplayName("Can get data from Movie model")
+    public void testNew() {
+        assertEquals("Die Hard", testMovie.getMovieName());
+    }
+}
