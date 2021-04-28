222aa241ba9dd62897aad6cef37d4f5a958bd83a
Lucas Morón <lucasmoron0512@gmail.com>
2021-04-28 20:29:29 -0300

    feat: Hibernate primer intento

--- src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/model/Reviewable.java
+++ src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/model/Reviewable.java
@@ -2,7 +2,6 @@ package ar.edu.unq.desapp.grupoL012021.backenddesappapl.model;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Entity
