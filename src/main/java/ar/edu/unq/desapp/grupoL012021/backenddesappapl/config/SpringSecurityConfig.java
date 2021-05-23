9814499de0e9fe29cb139f5ab14f0bf305dc4379
Emanuel <salgadoema@gmail.com>
2021-05-23 17:50:21 -0300

    fix: comunicacion con Front

--- src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/config/SpringSecurityConfig.java
+++ src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/config/SpringSecurityConfig.java
@@ -1,12 +1,18 @@
 package ar.edu.unq.desapp.grupoL012021.backenddesappapl.config;
 
 import ar.edu.unq.desapp.grupoL012021.backenddesappapl.model.JWTAuthorizationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
@@ -15,12 +21,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
+            http.cors(withDefaults());
             http.csrf().disable()
                     .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                     .authorizeRequests()
                     .antMatchers(HttpMethod.POST, "/api/users/signup/").permitAll()
                     .anyRequest().authenticated();
         }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
+    }
     }
 
 
