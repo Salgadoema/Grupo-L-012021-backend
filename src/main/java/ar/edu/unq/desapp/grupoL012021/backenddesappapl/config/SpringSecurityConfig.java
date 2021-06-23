7f4516a42a3e0221ae67cbf5d163572c09b009c8
Emanuel <esalgado@galileoar.com>
2021-06-23 19:36:24 -0300

    feat: actuator

--- src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/config/SpringSecurityConfig.java
+++ src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/config/SpringSecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -20,6 +22,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/v2/api-docs",
+                    "/configuration/ui",
+                    "/swagger-resources/**",
+                    "/configuration/security",
+                    "/swagger-ui.html",
+                    "/api/users/endponints",
+                    "/webjars/**",
+                    "/actuator/**");
+        }
+
+
+
+        @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.cors(withDefaults());
             http.csrf().disable()
@@ -28,8 +44,12 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers(HttpMethod.POST, "/api/users/signup/").permitAll()
                     .anyRequest().authenticated();
         }
-    @Bean
+
+
+
+        @Bean
     CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("*");
         configuration.addAllowedMethod("*");
--- src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/config/WebConfig.java
+++ src/main/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/config/WebConfig.java
@@ -1,7 +1,10 @@
 
 package ar.edu.unq.desapp.grupoL012021.backenddesappapl.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@Configuration
 //@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/actuator/**").permitAll()
+                .anyExchange().authenticated()
+                .and().build();
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
