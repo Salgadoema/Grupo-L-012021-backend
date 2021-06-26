d5629ef44c3e13297ac626ee615fc12449d84238
Emanuel <esalgado@galileoar.com>
2021-06-26 16:50:00 -0300

    fix: architecture changes

--- src/test/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/architecture/ArchitectureTest.java
+++ src/test/java/ar/edu/unq/desapp/grupoL012021/backenddesappapl/architecture/ArchitectureTest.java
@@ -24,14 +24,15 @@ public class ArchitectureTest {
 
     @Test
     public void restControllerInWebServicePackageTest() {
-        ArchRule rule = classes().that().areAnnotatedWith(Repository.class).should().resideInAPackage("..persistence..");
+        ArchRule rule = classes().that().areAnnotatedWith(RestController.class).should().resideInAPackage("..webservices..");
 
         rule.check(projectClasses);
     }
 
     @Test
-    public void persistenceInPersistencePackageTest() {
-        ArchRule rule = classes().that().areAnnotatedWith(RestController.class).should().resideInAPackage("..webservices..");
+    public void repositoryInPersistencePackageTest() {
+        ArchRule rule = classes().that().areAnnotatedWith(Repository.class).should().resideInAPackage("..persistence..");
+
 
         rule.check(projectClasses);
     }
